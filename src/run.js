f71a2e7ac75717cfe2c8ba38365c7be662d905dc
pillsilly <marcfrances@outlook.com>
2020-08-12 23:04:58 +0800

    npm

--- src/run.js
+++ src/run.js
@@ -1,12 +1,21 @@
 const unzipper = require('unzipper');
 const fs = require('fs');
-
-fs.createReadStream('./src/run.zip')
+const path = require('path');
+const zipPath = './src/run.zip';
+const unzipDir = `${path.dirname(zipPath)}/${path.basename(zipPath).split('.').shift()}`;
+fs.mkdirSync(unzipDir);
+fs.createReadStream(zipPath)
   .pipe(unzipper.Parse())
   .on('entry', function (entry) {
     const fileName = entry.path;
     const type = entry.type; // 'Directory' or 'File'
-      const size = entry.vars.uncompressedSize; // There is also compressedSize;
+    const size = entry.vars.uncompressedSize; // There is also compressedSize;
+    if (entry.type === 'Directory') {
+      fs.mkdirSync(`${unzipDir}/${entry.path}`)
+    } else { 
+      
+    }
+
       console.log(fileName);
     // if (fileName === "this IS the file I'm looking for") {
     //   entry.pipe(fs.createWriteStream('output/path'));
