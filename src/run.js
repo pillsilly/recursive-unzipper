be8e7c2b9fa8edef77e336da9940581ed4de989b
pillsilly <marcfrances@outlook.com>
2020-09-10 22:45:00 +0800

    asdf

--- src/run.js
+++ src/run.js
@@ -1,10 +1,20 @@
-const {ZipExplorer} = require('../src/ZipExplorer')
+const {ZipExplorer} = require('../src/ZipExplorer');
 
 module.exports = function run({file = ''}) {
-  const explorer = new ZipExplorer(file)
+  const explorer = new ZipExplorer(file);
+  console.info(`Extracting  ${file}`);
   explorer.getAllFiles()
     .then(files => {
       return files.filter(file => !file.isZip)
         .map(file => file.extractToDefault())
+    })
+    .then(() => {
+      console.info('Extraction completed')
+    })
+    .catch((e) => {
+      console.info(`Extraction failed ${e.stackTrace}`)
+    })
+    .finally(() => {
+      console.info('Done')
     })
 };
