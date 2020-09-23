cc2bf884f081795a71dfc36fae724f39f42623a8
pillsilly <marcfrances@outlook.com>
2020-09-23 22:20:27 +0800

    e2e test ok

--- src/run.js
+++ src/run.js
@@ -1,11 +1,11 @@
 const {ZipExplorer} = require('../src/ZipExplorer');
 
-module.exports = function run({file = ''}) {
+module.exports = function run({file = '', dir, name}) {
   const explorer = new ZipExplorer(file);
-  console.info(`Extracting  ${file}`);
+  console.info(`Extracting  ${file} with option dir=${dir} name=${name}`);
   explorer.getAllFiles()
     .then(files => {
-      return files.filter(file => !file.isZip)
+      return filterByDirAndName({files, dir, name})
         .map(file => file.extractToDefault())
     })
     .then(() => {
@@ -18,3 +18,32 @@ module.exports = function run({file = ''}) {
       console.info('Done')
     })
 };
+
+
+function filterByDirAndName({files, dir, name}) {
+  let filtered = files;
+
+  if (dir) {
+    dir = new RegExp(dir);
+    filtered = files.filter(byDir(dir))
+  }
+
+  if (name) {
+    name = new RegExp(name);
+    filtered = filtered.filter(byName(name))
+  }
+
+  return filtered;
+
+  function byDir(pathRegex) {
+    return (file) => {
+      return pathRegex.test(file.parentPath);
+    }
+  }
+
+  function byName(name) {
+    return (file) => {
+      return name.test(file.path);
+    }
+  }
+}
--- test/ZipExplorer.spec.js
+++ test/ZipExplorer.spec.js
@@ -1,9 +1,9 @@
-const {ZipExplorer} = require('../src/ZipExplorer')
+const {ZipExplorer} = require('../src/ZipExplorer');
 const fs = require('fs');
 const unZipper = require('unzipper');
 
 describe('Name of the group', () => {
-  const path = 'test/resource/run.zip'
+  const path = 'test/resource/run.zip';
 
   let explorer = new ZipExplorer(path);
   beforeEach(() => {
@@ -16,6 +16,48 @@ describe('Name of the group', () => {
     await new ZipExplorer(path).extractLZMACompressedFile(buffer, '1.txt')
   });
 
+  // Do you want to extract by dir/sub zip/file name
+  //
+  // Your desired dir contains
+  //
+  // Your desired sub-zip file name contains
+  //
+  // Your desired file name contains
+  //
+  // Above files would be extracted, continue?
+
+  test('should filter by regex', async () => {
+    explorer = new ZipExplorer('test/resource/snapshot.zip');
+    const files = await explorer.getAllFiles();
+    let filtered = [];
+
+    const dir = /snapshot.zip\/BTS1_1011_part_7.zip/;
+    const name = /\.xml\.gz/;
+
+    if(dir) {
+      filtered = files.filter(byDir(dir))
+    }
+
+    if(name) {
+      filtered = filtered.filter(byName(name))
+    }
+
+    function byDir(pathRegex) {
+      return (file) => {
+        return pathRegex.test(file.parentPath);
+      }
+    }
+
+    function byName(name) {
+      return (file) => {
+        return name.test(file.path);
+      }
+    }
+
+    const fileList = filtered.map(f => `${f.parentPath}/${f.path}`).join('\n');
+    fs.writeFileSync('result.txt', fileList, 'utf-8')
+  })
+
 
   test('should extract tar compressed file', async () => {
     const p = 'C:\\Users\\pills\\code\\InterractiveUnzipper\\test\\resource\\BTS1_123D_linux_logs.tar';
@@ -24,15 +66,15 @@ describe('Name of the group', () => {
   });
 
   test('should traverse all zip directory', async () => {
-    explorer = new ZipExplorer('test/resource/snapshot.zip')
-    const files = await explorer.getAllFiles()
+    explorer = new ZipExplorer('test/resource/snapshot.zip');
+    const files = await explorer.getAllFiles();
     await Promise.all(files.filter(f => !f.isZip)
       .map(f => f.extractToDefault())
     );
   });
 
   test('should work for snapshot files', async () => {
-    explorer = new ZipExplorer('test/resource/snapshot.zip')
+    explorer = new ZipExplorer('test/resource/snapshot.zip');
     const list = await explorer.listAll();
 
     const childrenZip = await list[0].getDirectory();
