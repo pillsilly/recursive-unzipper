5f8e4f4c05545f5987d221758a4793ff9401f462
pillsilly <marcfrances@outlook.com>
2022-09-06 21:44:49 +0800

    readme

--- src/bin.ts
+++ src/bin.ts
@@ -14,13 +14,12 @@ function getOptions() {
     .usage('[global options]')
     .requiredOption(
       '-f --file [file]',
-      'Path of the file to be extract'
+      'Path of the files to be extract'
     )
-    .option('-d --dir [dir]', 'The dir name to filter with')
-    .option('-ds --dest [destination directory]', 'The destination dir path')
+    .option('-d --dir [dir]', 'A directory name to filter with')
+    .option('-ds --dest [destination directory]', 'The destination directory where file will be extracted; if not specified, a same name directory will be created aside of the zip file as the "destination directory"')
     .option(
-      '-n --name [name]',
-      'The file name to filter with'
+      '-n --name [name]', 'A file name to filter with'
     )
     .parse(process.argv);
   const opts = program.opts() as Parameters<typeof run>[0];
