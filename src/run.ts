da9ca10866762bfef50353b4c9bea84a1365f1f9
pillsilly <marcfrances@outlook.com>
2022-09-16 23:37:22 +0800

    fix-pre-minor

--- src/run.ts
+++ src/run.ts
@@ -2,9 +2,7 @@ import {Extractor, logger} from './Extractor';
 
 async function run({file = '', dir = '', name = '', dest = ''}) {
   const extractor = new Extractor(file, dest);
-  await extractor.extract().finally(() => {
-    logger.info('Ended');
-  });
+  await extractor.extract();
 }
 
 export {run};
