7942a245f20343e824a3540d4c1b779c21b9b146
pillsilly <marcfrances@outlook.com>
2023-04-17 22:40:31 +0800

    esbuild above jest

--- jest.config.js
+++ jest.config.js
@@ -165,7 +165,16 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    // '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': [
+      'esbuild-jest',
+      {
+        sourcemap: true,
+        loaders: {
+          '.spec.ts': 'tsx',
+        },
+      },
+    ],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
